ce46fd95c14c5e086bf6dec4f3859ae94215cba4
lucasoliveiraa <lucasjackcosta18@gmail.com>
2022-06-12 23:18:37 -0300

    chore: create resolvers graphql

--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -1,7 +1,22 @@
+import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import path from 'node:path';
+
+import { DatabaseModule } from '../database/database.module';
+import { ProductsService } from '../services/products.service';
+import { ProductsResolver } from './graphql/resolvers/products.resolver';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  providers: [ProductsResolver, ProductsService],
 })
 export class HttpModule {}
